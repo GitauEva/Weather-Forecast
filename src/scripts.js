0d8a0460fb11bf6974ac38700e5cd4981b1ffb74
yvagita <gitauenjeri@gmail.com>
2024-09-03 11:13:17 +0300

    Added API Functionality to pull weather data

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,19 @@
 let searchForm = document.querySelector("#search-form");
 
+function searchCity(city) {
+  let apiKey = "f3bc84a5t55d232e835bfe4a96o890e3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let temperatureValue = response.data.temperature.current;
+  let weatherValue = document.querySelector("#weather-value");
+
+  temperatureValue.innerHTML = weatherValue;
+}
+
 function displayInfo(event) {
   event.preventDefault();
 
@@ -7,8 +21,10 @@ function displayInfo(event) {
   let search = document.querySelector("#search");
   let searchValue = search.value;
   //Getting the HTML to be able to display the Value from the search bar
+
   let cityDisplay = document.querySelector("#main-city");
   cityDisplay.innerHTML = searchValue;
+  searchCity(searchValue);
 }
 
 searchForm.addEventListener("submit", displayInfo);
